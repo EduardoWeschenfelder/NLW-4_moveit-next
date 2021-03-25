6441b1d58522bfb4e850b595f018fb3789db32c5
EduardoWeschenfelder <eduardoweschenfelder@alunos.utfpr.edu.br>
2021-03-25 00:10:28 -0300

    aula 3
    Regras da aplicação e useContext React

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,46 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/challengesContext";
+import styles from "../styles/components/ChallengeBox.module.css";
+export function ChallengeBox() {
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {activeChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe {activeChallenge.amount} xp</header>
+          <main>
+            {/* {activeChallenge.type === "body" ? (
+              <img src="icons/body.svg" alt="" />
+            ) : (
+              <img src="icons/eye.svg" alt="" />
+            )} */}
+            <img src={`icons/${activeChallenge.type}.svg`} alt="" />
+            <strong>Novo Desafio</strong>
+            <p>{activeChallenge.description}</p>
+          </main>
+          <footer>
+            <button
+              type="button"
+              className={styles.challengeFailedButton}
+              onClick={resetChallenge}
+            >
+              Falhei
+            </button>
+            <button type="button" className={styles.challengeSucceedButton}>
+              Completei
+            </button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Inicie um ciclo para receber desafios</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level up" />
+            Avance de level completando desafios
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
